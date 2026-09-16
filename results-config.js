9524681b45ef09afcdd1108cd619b711995d7120
XinLi6789 <taylorlee0614@gmail.com>
2026-09-16 20:19:21 +0700

    Connect automatic result submissions

--- results-config.js
+++ results-config.js
@@ -1,3 +1,3 @@
 // Paste the deployed Google Apps Script /exec URL between the quotes.
 // Leave blank to use the downloadable-results fallback.
-window.HANZI_RESULTS_ENDPOINT = "";
+window.HANZI_RESULTS_ENDPOINT = "https://script.google.com/macros/s/AKfycbxfFi4GnZp_uFTdR16jCefO8D1cRzz0hBO-C5ACpM8GPMN3k2M3tRWYL3Eckgjf7sUr/exec";
